0e40b08665d8bf3cd81df8b10d950803b34f2582
KoCTbI4 <116780591+KoCTbI4@users.noreply.github.com>
2025-10-18 13:38:48 +0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -24,7 +24,7 @@ uploadInput.addEventListener('change', async (e) => {
   addMessageToChat('bot', 'Обработка...');
 
   try {
-    const response = await fetch('https://noncontinuably-meatier-ardella.ngrok-free.dev/api/describe', {
+    const response = await fetch('https://noncontinuably-meatier-ardella.ngrok-free.dev', {
       method: 'POST',
       body: formData,
     });
